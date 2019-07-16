4f3d51b3b8ecf2a6abd016f26f5b881e9f0cb82b
flyash <fenxuehaiyan@qq.com>
2019-07-16 15:24:14 +0800

    Clean & optimize

--- themes/maupassant/source/js/totop.js
+++ themes/maupassant/source/js/totop.js
@@ -1,12 +1,7 @@
 $(window).scroll(function() {
-    $(window).scrollTop() > 500 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
+    $(window).scrollTop() > 500 ? $("#toTop").addClass("show") : $("#toTop").removeClass("show");
 });
-$("#rocket").click(function() {
-    $("#rocket").addClass("launch");
-    $("html, body").animate({
-        scrollTop: 0
-    }, 500, function() {
-        $("#rocket").removeClass("show launch");
-    });
+$("#toTop").click(function() {
+    $("html, body").animate({scrollTop: 0}, 500);
     return false;
 });
