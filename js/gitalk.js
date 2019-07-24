f839ce09a3b58f5e147b96c707eedc0f0baa1aeb
flyash <fenxuehaiyan@qq.com>
2019-07-24 15:26:35 +0800

    Site updated: 2019-07-24 15:26:33

--- js/gitalk.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  if (!window.gitalkConfig) return;
-  var onload = window.onload;
-  window.onload = function() {
-    if (onload) onload();
-    var gitalk = new Gitalk(window.gitalkConfig);
-    gitalk.render('gitalkWrapper');
-  }
-})();
